d7b4e8b99fc2e4f2126f06769c2988eee32f54cc
Kilobato04 <134810631+Kilobato04@users.noreply.github.com>
2026-08-12 18:24:07 -0600

    Update index.js

--- lambda/generate-alert/index.js
+++ lambda/generate-alert/index.js
@@ -488,7 +488,7 @@ async function captureCisternaPanel(browser, targetUrl) {
     console.log(`🔗 Navegando a Cisterna: ${targetUrl}`);
     const page = await browser.newPage();
     
-    await page.setViewport({ width: 480, height: 1800, deviceScaleFactor: 2 });
+    await page.setViewport({ width: 820, height: 1800, deviceScaleFactor: 2 });
     
     await page.goto(targetUrl, { waitUntil: 'networkidle2', timeout: 45000 });
     
